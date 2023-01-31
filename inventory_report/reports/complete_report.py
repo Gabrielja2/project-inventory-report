ea4fb717fa9b65b2759f4666a31795e801c70189
Gabriel <gabrielja2antunes@gmail.com>
2023-01-31 19:22:36 -0300

    🚀 features: implementado class CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,16 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, list):
+        simple_report = super().generate(list)
+
+        companies_counter = cls.get_companies(list)
+
+        description = "\nProdutos estocados por empresa:\n"
+
+        for company, quantity in companies_counter.items():
+            description += f"- {company}: {quantity}\n"
+
+        return simple_report + description
