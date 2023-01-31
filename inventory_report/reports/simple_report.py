82245b86f7edff18497d02a0fbb55b3b640c75d4
Gabriel <gabrielja2antunes@gmail.com>
2023-01-31 17:42:34 -0300

    🚀 features: implementado a class SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,30 @@
+from datetime import datetime
+from ast import Dict
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    def generate(list: Dict):
+        # nome da empresa
+        companies = [product["nome_da_empresa"] for product in list]
+        companie_most_common = Counter(companies).most_common()[0][0]
+
+        # data de fabricação
+        manufacturing_dates = [
+            product["data_de_fabricacao"] for product in list
+        ]
+        min_manufacturing_date = min(manufacturing_dates)
+
+        # data de validade
+        date_now = str(datetime.now())
+        expiration_dates = [
+            product["data_de_validade"] for product in list
+            if product["data_de_validade"] >= date_now
+        ]
+        next_expiration_date = min(expiration_dates)
+
+        return (
+            f"Data de fabricação mais antiga: {min_manufacturing_date}\n"
+            f"Data de validade mais próxima: {next_expiration_date}\n"
+            f"Empresa com mais produtos: {companie_most_common}"
+        )
