24d8783b140616877c0846b74fb01aebfcae1366
Gabriel <gabrielja2antunes@gmail.com>
2023-01-31 20:12:30 -0300

    🚀 features: criado metodo open_csv da Inventory

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,23 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+import csv
+
+
+class Inventory:
+    @classmethod
+    def import_data(self, path, tipo):
+        if "csv" in path:
+            return Inventory.open_csv(path, tipo)
+        elif "json" in path:
+            return Inventory.open_json(path, tipo)
+
+        return Inventory.open_xml(path, tipo)
+
+    @classmethod
+    def open_csv(self, path, tipo):
+        with open(path, encoding="utf-8") as file:
+            inventory_file = csv.DictReader(file, delimiter=",", quotechar='"')
+            if tipo == "simples":
+                return SimpleReport.generate(list(inventory_file))
+
+            return CompleteReport.generate(list(inventory_file))
